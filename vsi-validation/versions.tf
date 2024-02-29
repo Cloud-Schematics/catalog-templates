1aa82978d96ce7fa87fb9a7c7dcd38795a53cd40
bsvihovec <bsvihovec@yahoo.com>
2024-02-29 12:05:25 -0500

    Update versions.tf

--- vsi-validation/versions.tf
+++ vsi-validation/versions.tf
@@ -1,8 +1,8 @@
 terraform {
     required_providers {
         ibm = {
-            source = "IBM-Cloud/ibm"
-            version = "~> 1.62.0"
+            source = "jonwoodlief/catalog"
+            version = "~> 3.1.1"
         }
     }
 }
